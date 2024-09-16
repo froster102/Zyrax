c6c2d313ad394ebb7a515b212752fe4c4eaac642
froster441 <froster441@gmail.com>
2024-09-16 11:50:12 +0530

    commit

--- client/src/components/OrderSucess.jsx
+++ client/src/components/OrderSucess.jsx
@@ -2,12 +2,14 @@ import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { selectActiveGender } from '../features/userSlice'
+import queryString from 'query-string'
 
 function OrderSucess() {
     const navigate = useNavigate()
     const location = useLocation()
     const activeGender = useSelector(selectActiveGender)
-    console.log(location.search)
+    const { order_id } = queryString.parse(location.search.toString())
+
     // useEffect(() => {
     //     if (!location?.state?.orderSucess) {
     //         navigate(`/${activeGender}`)
@@ -19,7 +21,7 @@ function OrderSucess() {
             <div className='w-full flex justify-center mt-24 mb-24' >
                 <div>
                     <div className='h-fit bg-black p-10 rounded-lg flex items-center' >
-                        <img className='w-40' src="/dummy/cargo.png" alt="" />
+                    <img className='w-40' src="/dummy/cargo.png" alt="" />
                         <h1 className='text-white font-extrabold text-4xl ml-4 text-center' >Order sucessfull</h1>
                     </div>
                     <div className='w-full pt-6 flex justify-center'>
