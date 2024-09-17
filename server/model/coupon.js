43413c48a47e2efb46910fd040f0cf930ba0dc98
froster102 <136457230+froster102@users.noreply.github.com>
2024-09-17 18:51:13 +0530

    coupon model

---
+++ server/model/coupon.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const CouponSchema = new mongoose.Schema({
+    code: {
+        type: String,
+        unique: [true, 'Coupon code must be unique'],
+        required: [true, 'Coupon code is required'],
+        set: value => value.toUpperCase()
+    },
+    dicount: {
+        type: Number,
+        required: [true, 'Dicount is percentage is required'],
+        min: [1, 'Dicount must be at least 1%'],
+        max: [100, 'Dicount cannot exceed 100%']
+    },
+    expirationDate: {
+        type: Date,
+        required: [true, 'Expiration date is required']
+    },
+    minPurchaseAmount: {
+        type: Number,
+        required: [true, 'Minimum purchase amount is required']
+    },
+    usageLimitPerUser: {
+        type: Number,
+        default: 1
+    },
+    totalUsageLimit: {
+        type: Number,
+        default: Infinity
+    },
+    status: {
+        type: String,
+        default: 'active',
+        enum: {
+            values: ['active', 'blocked'],
+            message: 'Coupon is status should be either "active" or "blocked"'
+        }
+    },
+    usageRecords: [{
+        user: {
+            type: mongoose.SchemaTypes.ObjectId,
+            ref: 'User',
+        },
+        userAt: {
+            type: Date,
+            default: Date.now()
+        }
+    }]
+})
+
+const Coupon = mongoose.model('Coupon', CouponSchema)
+
+export {
+    Coupon
+}
