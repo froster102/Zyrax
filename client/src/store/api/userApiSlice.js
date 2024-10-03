95b87880994d683a40b55dfc22d262f0ec521634
froster102 <136457230+froster102@users.noreply.github.com>
2024-10-03 21:03:40 +0530

    cache invalidation

--- client/src/store/api/userApiSlice.js
+++ client/src/store/api/userApiSlice.js
@@ -32,45 +32,52 @@ const userApiSlice = apiSlice.injectEndpoints({
                 method: 'DELETE',
             })
         }),
+        getUserWishlistItems: builder.query({
+            query: () => '/user/wishlist',
+            providesTags: (result) => result ? [{ type: 'Wishlists', id: 'LIST' }] : []
+        }),
         addItemsToUserWishlist: builder.mutation({
-            query: ({ productId, action = '', productIds = [] }) => ({
+            query: ({ items = [] }) => ({
                 url: '/user/wishlist/items',
                 method: 'POST',
-                body: { productId, action, productIds }
-            })
-        }),
-        getUserWishlistItems: builder.query({
-            query: () => '/user/wishlist'
+                body: { items }
+            }),
+            invalidatesTags: [{ type: 'Wishlists', id: 'LIST' }]
         }),
         removeItemFromUserWishlist: builder.mutation({
             query: ({ itemId }) => ({
                 url: `/user/wishlist/items/${itemId}`,
                 method: 'DELETE'
-            })
+            }),
+            invalidatesTags: [{ type: 'Wishlists', id: 'LIST' }]
         }),
         getItemsFromUserCart: builder.query({
-            query: () => '/user/cart'
+            query: () => '/user/cart',
+            providesTags: (result) => result ? [{ type: 'CartItems', id: 'LIST' }] : []
         }),
         addItemsToUserCart: builder.mutation({
             query: ({ items }) => ({
                 url: '/user/cart/items',
                 method: 'POST',
                 body: { items }
-            })
+            }),
+            invalidatesTags: [{ type: 'CartItems', id: 'LIST' }]
         }),
         updateUserCartItems: builder.mutation({
             query: ({ itemId, selectedSize, selectedQty, index }) => ({
                 url: `/user/cart/items/${itemId}`,
                 method: 'PUT',
                 body: { selectedQty, selectedSize, index }
-            })
+            }),
+            invalidatesTags: [{ type: 'CartItems', id: 'LIST' }]
         }),
         removeItemFromUserCart: builder.mutation({
             query: ({ itemId, selectedSize }) => ({
                 url: `/user/cart/items/${itemId}`,
                 method: 'DELETE',
                 body: { selectedSize }
-            })
+            }),
+            invalidatesTags: [{ type: 'CartItems', id: 'LIST' }]
         }),
         chekout: builder.mutation({
             query: ({ paymentMethod, shippingAddressId, cardDetails }) => {
@@ -89,26 +96,36 @@ const userApiSlice = apiSlice.injectEndpoints({
             })
         }),
         fetchUserOrders: builder.query({
-            query: () => '/user/orders'
+            query: () => '/user/orders',
+            providesTags: (result) => result ? [{ type: 'UserOrders', id: 'LIST' }] : []
         }),
         getUserOrderDetails: builder.query({
             query: ({ orderId, productId }) => {
                 if (orderId && productId) return `/user/orders/details?orderId=${orderId}&productId=${productId}`
                 return `/user/orders/details?orderId=${orderId}`
-            }
+            },
+            providesTags: [{ type: 'OrderDetails', id: 'Details' }]
         }),
         cancelOrder: builder.mutation({
             query: ({ orderId, productId }) => ({
                 url: `/user/orders/${orderId}/products/${productId}/cancel`,
                 method: 'PATCH',
-            })
+            }),
+            invalidatesTags: [
+                { type: 'UserOrders', id: 'LIST' },
+                { type: 'OrderDetails', id: 'Details' }
+            ],
         }),
         returnOrder: builder.mutation({
             query: ({ orderId, productId, additionalRemarks, reason }) => ({
                 url: `/user/orders/${orderId}/products/${productId}/return`,
                 method: 'POST',
                 body: { additionalRemarks, reason }
-            })
+            }),
+            invalidatesTags: [
+                { type: 'UserOrders', id: 'LIST' },
+                { type: 'OrderDetails', id: 'Details' }
+            ]
         }),
         getWalletDetails: builder.query({
             query: () => '/user/wallet'
