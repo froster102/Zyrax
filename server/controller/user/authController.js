9c04d25350bb722c9e486a3597c5355540cf0a5d
froster102 <136457230+froster102@users.noreply.github.com>
2024-10-17 19:07:38 +0530

    unwanted logout remove

--- server/controller/user/authController.js
+++ server/controller/user/authController.js
@@ -183,15 +183,4 @@ export const verifyEmail = async (req, res) => {
     })
 }
 
-// @desc Verify user email
-// @routes POST /api/v1/user/auth/reset-password
-// @access Public
-export const logout = (req, res) => {
-    const token = req.cookies.jwt
-    if (!token) {
-        return res.status(401).json({ message: 'Unauthorised Access' })
-    }
-    res.clearCookie('jwt', { httpOnly: true, secure: false })
-    return res.status(200).json({ message: 'Logged out sucessfully' })
-}
 
