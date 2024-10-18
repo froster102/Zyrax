7e093ac4d143c16e82d35996f696f52c989a6857
froster102 <froster441@gmail.com>
2024-10-18 13:15:42 +0000

    image src change

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -2,7 +2,7 @@ function Banner() {
     return (
         <>
             <div className="mt-4 w-full lg:h-[589px] md:h-[489px] sm:h-[335px] h-[235px]">
-                <img className="h-full w-full object-cover" src="/dummy/rayul-_M6gy9oHgII-unsplash.jpg" alt="" />
+                <img className="h-full w-full object-cover" src="/rayul-_M6gy9oHgII-unsplash.jpg" alt="" />
             </div>
         </>
     )
